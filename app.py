e5342cd066b24fad117a1960a0aadd0d7173022d
Karen <ka.spk27@gmail.com>
2023-03-08 20:25:47 +0300

    gpt-3.5-turbo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from telebot import TeleBot, types
-from functions import respotnse_gpt, check_user
+from functions import respotnse_gpt, check_user, dell_db
 import openai
 import os
 from dotenv import load_dotenv
@@ -12,19 +12,21 @@ openai.api_key = os.getenv("CHAT_GPT3_API_KEY")
 bot = TeleBot(os.getenv('TELEGRAM_BOT_TOKEN'))
 
 item1 = types.KeyboardButton('/start')
-item2 = types.KeyboardButton('/help')
+item2 = types.KeyboardButton('/help') 
+item3 = types.KeyboardButton('/deletecontext') 
+
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     if check_user(message.from_user.id, USERS_ID):
-        markup.add(item1, item2)
+        markup.add(item1, item2, item3)
         bot.send_message(message.chat.id, "Привет, {0.first_name}!\
             \nОтправь сообщение с вопросом для ChatGPT.\
             ".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
     else:
-        markup.add(item1, item2)
+        markup.add(item1, item2, item3)
         bot.send_message(message.chat.id, "Привет, {0.first_name}!\
             \nДля доступа к чату тебе нужно получить доступ у админа.\
             ".format(message.from_user), parse_mode='html', reply_markup=markup)
@@ -33,28 +35,44 @@ def start(message):
 @bot.message_handler(commands=['help'])
 def help(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(item1, item2)
-    bot.reply_to(message, "Админ @karen_av",reply_markup=markup)
+    markup.add(item1, item2, item3)
+    bot.send_message(message.chat.id, "Админ @karen_av",reply_markup=markup)
 
 
+
+@bot.message_handler(commands=['deletecontext'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    if check_user(message.from_user.id, USERS_ID):
+        markup.add(item1, item2, item3)
+        dell_db(message.from_user.id)
+        bot.send_message(message.chat.id, "Контекст сброшен", reply_markup=markup)
+    else:
+        markup.add(item1, item2, item3)
+        bot.send_message(message.chat.id, "\
+            \nДля доступа к чату тебе нужно получить доступ у админа.\
+            ".format(message.from_user), parse_mode='html', reply_markup=markup)
+        
 
 @bot.message_handler(content_types=['text'])
 def text(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     if message.chat.type == 'private' and check_user(message.from_user.id, USERS_ID):
-        markup.add(item1, item2)
+        markup.add(item1, item2, item3)
         try:
-            response = respotnse_gpt(message.text)
-            output_text = response.choices[0].text
-            bot.send_message(message.chat.id, output_text, reply_markup=markup)
+            response = respotnse_gpt(message.from_user.id, message.text)
+            #output_text = response.choices[0].text
+            bot.send_message(message.chat.id, response, reply_markup=markup)
         except Exception as _ex:
             bot.send_message(message.chat.id, "Ошибка. Обратитесь к системному администратору.", reply_markup=markup)
             print(f'Exeption: {_ex}')
     else:
-        markup.add(item1, item2)
+        markup.add(item1, item2, item3)
         bot.send_message(message.chat.id, "\
             \nДля доступа к чату тебе нужно получить доступ у админа.\
             ".format(message.from_user), parse_mode='html', reply_markup=markup)
+        
+        
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
--- functions.py
+++ functions.py
@@ -1,16 +1,96 @@
 import openai
+import sqlite3
 
-def respotnse_gpt(query):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=query,
-        max_tokens=1000
-    )
-    return response
 
 def check_user(user_id, USERS_ID):
     if str(user_id) in USERS_ID:
         return True
     return False
 
+
+def connection_db():
+    try:
+        connection = sqlite3.connect('sqlite.db')
+        cursor = connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS gpt (
+                                    id INTEGER PRIMARY KEY,
+                                    user_id TEXT,
+                                    assistant TEXT);''')
+        cursor.close()
+        return connection
+    except:
+        return False
+
+
+def insert_db(user_id, assist_text):
+    val = (user_id, assist_text,)
+    connection = connection_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO gpt (user_id, assistant)  VALUES  (?, ?)", val)
+        connection.commit()
+        cursor.close()
+    except Exception as _ex:
+            print("[INFO] Error while working with SQLite", _ex)
+    finally:
+        if connection:
+            connection.close()
+            print("[INFO] SQLite connection closed")
+
+
+def select_db(user_id):
+    user_id = (user_id, )
+    connection = connection_db()
+    try:
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT assistant FROM gpt WHERE user_id=?", user_id).fetchall()
+        cursor.close()
+        return result
+    except Exception as _ex:
+            print("[INFO] Error while working with SQLite", _ex)
+    finally:
+        if connection:
+            connection.close()
+            print("[INFO] SQLite connection closed")
+
+
+def dell_db(user_id):
+    user_id = (user_id, )
+    connection = connection_db()
+    try:
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM gpt WHERE user_id=?", user_id)
+        connection.commit()
+        cursor.close()
+    except Exception as _ex:
+            print("[INFO] Error while working with SQLite", _ex)
+    finally:
+        if connection:
+            connection.close()
+            print("[INFO] SQLite connection closed")
+
+
+def respotnse_gpt(user_id, content):
+    messages = [{"role": "system", "content" : "You're a kind helpful assistant. Answer as concisely as possible."}]
+    assistant = select_db(user_id)
+    for assist in assistant:
+         print(f'assist - {assist[0]}')
+         messages.append({"role": "assistant", "content": assist[0]})
+    messages.append({"role": "user", "content": content})
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages=messages
+    )
+    chat_response = completion.choices[0].message.content
+    print(f'ChatGPT: {chat_response}')
+    insert_db(user_id, chat_response)
+    return chat_response
+
+
+    # Максимальное количество токенов для генерации при завершении (здесь вы можете увидеть токенизатор, который использует OpenAI) https://platform.openai.com/tokenizer
+    #max_tokens=1000
+    # Используемая температура отбора проб. Значения, близкие к 1, придадут модели больше риска / креативности, в то время как значения, близкие к 0, будут генерировать чётко определённые ответы.
+    # Количество вариантов завершения общения, которые необходимо сгенерировать для каждого входного сообщения.
+    #temperature = 0.7
+    #n = 1
 
